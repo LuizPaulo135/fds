92eb2f79901f4dbd1b0e990e5abdacb05a081567
LuizPaulo135 <luiz_pipaulo@hotmail.com>
2025-07-02 21:57:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function mostrarMensagem() {
   const areaMensagem = document.getElementById("mensagem");
   areaMensagem.textContent = "Seja bem-vindo(a)! Nunca pare de aprender. 🌟";
 }
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #2e8b57;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  margin: 10px 0;
+}
+
+button:hover {
+  background-color: #246b45;
+}
