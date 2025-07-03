abe41cceeab21c9b06201c96bd541a16b812f81d
LuizPaulo135 <luiz_pipaulo@hotmail.com>
2025-07-02 22:24:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function mostrarMensagem() {
   const areaMensagem = document.getElementById("mensagem");
   areaMensagem.textContent = "Seja bem-vindo(a)! DEUS LHE ABENÇOE ";
-  areaMensagem.classList.add("mostrar"); // ativa o fade in
+
 }
