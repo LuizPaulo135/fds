8293bbb1e6ce3f67135a1508da177cace88870d7
LuizPaulo135 <luiz_pipaulo@hotmail.com>
2025-07-02 22:07:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-"""function mostrarMensagem() {
-  const areaMensagem = document.getElementById("mensagem");
-  areaMensagem.textContent = "Seja bem-vindo(a)! Nunca pare de aprender. 🌟";
-}
-"""
+<script>
+  function mostrarMensagem() {
+    const areaMensagem = document.getElementById("mensagem");
+    areaMensagem.textContent = "Seja bem-vindo(a)! Nunca pare de aprender. 🌟";
+  }
+</script>
